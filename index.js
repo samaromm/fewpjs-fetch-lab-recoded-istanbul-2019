e22430ae8e9d7063e3426dc68d1b8b7058ba42fd
Samar Omar <samaromm98@gmail.com>
2020-01-28 15:50:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function fetchBooks() {
 
+function fetchBooks() {
+    fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
+
+
 
 function renderBooks(json) {
   const main = document.querySelector('main')
