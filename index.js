198e4056c30c2f757c61c56258d4c2390a40786b
Samar Omar <samaromm98@gmail.com>
2020-01-28 15:57:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function fetchBooks() {
 
+function fetchBooks() {
+    return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
+
+
 
 function renderBooks(json) {
   const main = document.querySelector('main')
